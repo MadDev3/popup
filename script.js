69318ac898fc219e03879390162c3e2c26a1d2e6
MadDev3 <vmakeev.dev@gmail.com>
2022-01-17 18:30:12 +0400

    popup

---
+++ script.js
@@ -0,0 +1,13 @@
+let popup = document.getElementById('window'),
+	popupToggle = document.getElementById('contact'),
+	popupClose = document.querySelector('.close');
+
+function Show(){
+	document.getElementById('window').style.display = "block";
+}
+
+function Close(){
+	document.getElementById('window').style.display = "none";
+}
+
+
